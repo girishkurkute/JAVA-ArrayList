21166de99d394a15b1646996dc23cb0cb231335f
Girish <gkurkut1@binghamton.edu>
2017-09-09 21:45:43 -0400

    File reading done

--- myArrayList/src/myArrayList/driver/Driver.java
+++ myArrayList/src/myArrayList/driver/Driver.java
@@ -1,9 +1,46 @@
 package myArrayList.driver;
 
+import java.util.InputMismatchException;
+
+import myArrayList.util.FileProcessor;
+
 public class Driver {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		String inputfile,outputfile;
+		String currNumber;
 		
+		if(args.length !=2)
+		{
+			
+			throw new RuntimeException("Please provide 2 arguments");
+		}
+		else
+		{
+			inputfile = args[0];
+			outputfile = args[1];
+			
+		}
+		
+		FileProcessor fpObj = new FileProcessor(inputfile);
+		
+		while((currNumber = fpObj.readLine())!= null)
+		{
+			int number = Integer.parseInt(currNumber);
+			System.out.println(currNumber);
+			if(number < 0 || number > 10000)
+			{
+				
+				fpObj.disposeScanner();
+				throw new InputMismatchException("Invalid number "+number);
+			}
+			else
+			{
+				//method to insert element
+			}
+			
+		
+		}
 
 	}
 
--- myArrayList/src/myArrayList/util/FileProcessor.java
+++ myArrayList/src/myArrayList/util/FileProcessor.java
@@ -1,6 +1,53 @@
 package myArrayList.util;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class FileProcessor {
 
+private Scanner scn = null;
 	
+
+	
+	public FileProcessor(String inputfile)throws Exception {
+		// TODO Auto-generated constructor stub
+		scn = new Scanner(new File(inputfile));
+	}
+
+	public String readLine() throws FileNotFoundException
+	{
+		try
+		{
+			String currLine;
+			if(scn == null)
+			{
+				throw new RuntimeException("File closed");
+				
+			}
+			while(scn.hasNext())
+			{
+				currLine = scn.next();
+				return currLine;
+			}
+			currLine = null;
+			disposeScanner();
+			return currLine;
+		}
+		catch(Exception e)
+		{
+			disposeScanner();
+			throw e;
+		}
+	}
+
+	public void disposeScanner() 
+	{
+		if(scn != null)
+		{
+			scn.close();
+			scn = null;
+		}
+		
+	}
+
 }
